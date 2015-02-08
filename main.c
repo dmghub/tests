040f5782209c43e41a12a5068ab70027762cb433
root <root@localhost.localdomain>
2015-02-08 14:49:22 +0200

    expand

--- main.c
+++ main.c
@@ -2,19 +2,44 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/resource.h>
 
 int main(int argc, char **argv) {
 
-
+    struct rlimit rl;
     pid_t pid;
+    
+    umask(0);
+    
+    if (getrlimit(RLIMIT_NOFILE, &rl) < 0)
+	printf("Cant't get num of limits for files\n");
     
     if ((pid = fork()) < 0) {
 	printf("Error fork() execution\n");
 	exit(1);
     } else if (pid == 0) {
 	/* in child */
+	int i;
+	int fd0, fd1, fd2;
+	
 	printf("My new PID: %d\n", getpid());
-	exit(0);
+
+	if (rl.rlim_max == RLIM_INFINITY)
+	    rl.rlim_max = 1024;
+
+	for (i = 0; i < rl.rlim_max; ++i)
+		close(i);
+	
+	fd0 = open("/dev/null", O_RDWR);
+	fd1 = dup(0);
+	fd2 = dup(0);
+	
+	
+	
+//	exit(0);
+
+	while (1) ;
+
     } else {
 	/* parent */
 	printf("Parent is died\n");
